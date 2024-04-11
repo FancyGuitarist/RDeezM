fa4a888926099133dff92fcab762bb381b19f340
FancyGuitarist <simf3010@gmail.com>
2024-04-11 14:37:26 -0400

    Cross the streams
    Split on da haters

--- Chargement.py
+++ Chargement.py
@@ -2,46 +2,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import numpy.polynomial.polynomial as poly
+from scipy import integrate
+from scipy.integrate import cumtrapz
 
 title = 'lift_2'
 data = pd.read_csv(f'{title}.csv', delimiter= ',')
 
 pos = data['Position (m)'].values
-charge_m = data['   Chargement (N/m)'].values
+w = data['   Chargement (N/m)'].values
 
-charge = []
+V = integrate.cumtrapz(w,pos,initial = 0)
+M = integrate.cumtrapz(V,pos,initial=0)
+
+
+"""charge = []
 M = []
-
-for index, value in enumerate(charge_m):
+fit_charge = poly.polyfit(pos,charge_m,20)"""
+"""for index, value in enumerate(charge_m):
     charge.append(pos[index]*value)
-    M.append((pos[index]**2)*value)
+    M.append((pos[index]**2)*value)"""
 
 
-fit_M = poly.polyfit(pos,M,120)
-fit_charge = poly.polyfit(pos,charge,120)
-x_fit = np.linspace(0,1.51,2000)
-def make_poly(fit_values, x_values):
+
+"""fit_M = poly.polyfit(pos,M,120)
+fit_charge = poly.polyfit(pos,charge,120)"""
+#x_fit = np.linspace(0,1.51,2000)
+def make_poly(fit_coeffs, symbol):
     y_fit = 0
-    for index, value in enumerate(fit_values):
-        y_fit += value*(x_values**index)
+    for index, value in enumerate(fit_coeffs):
+        y_fit += value*(symbol**index)
     return y_fit
 
-y_fit_charge = make_poly(fit_charge, x_fit)
-y_fit_M = make_poly(fit_M,x_fit)
-
-M_max = np.max(y_fit_M)
-V_max = np.max(y_fit_charge)
-
+#y_fit_charge = make_poly(fit_charge, x_fit)
+#y_fit_M = make_poly(fit_M,x_fit)
+#M = np.trapz(y_fit_charge,x_fit)
+#M_max = np.max(y_fit_M)
+#V_max = np.max(y_fit_charge)
+"""x = Symbol('x')
+sigma = make_poly(fit_charge,x)
+V = integrate(sigma,x)
+M = integrate(V, x)"""
 
 #area_M = np.trapz(y_fit_M, x_fit)
 #area_charge = np.trapz(y_fit_charge,x_fit)
-print(f'M = {M_max}, V = {V_max}')
+#print(f'M = {M}, V = {V_max}')
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-#ax.plot(pos, charge, color = 'Orange')
-ax.plot(x_fit, y_fit_charge, color = 'Red')
-ax.plot(x_fit, y_fit_M, color = 'Blue')
+ax.plot(pos, V, color = 'Black')
+ax.plot(pos, M, color = 'Orange')
+ax.plot(pos, w, color = 'Blue')
+#ax.plot(x_fit, y_fit_charge, color = 'Red')
+#ax.plot(x_fit, y_fit_M, color = 'Blue')
 fig.tight_layout()
 fig.subplots_adjust(top=0.9)
 plt.xlabel("position")
--- Etude_Cas.py
+++ Etude_Cas.py
@@ -38,21 +38,52 @@ def make_poly(fit_values, x_values):
 y_bottom_fit = make_poly(fit_bottom, x_fit)
 y_top_fit = make_poly(fit_top, x_fit)
 
+y_cen = 0.053017
 #Calculate Area of Wing Ding ding
 area_bottom = np.trapz(y_bottom_fit, x_fit)
-area_top = np.trapz(y_top_fit, x_fit)
-area = area_top - area_bottom
+cutoff = []
+prev_val = 0
+for index, value in enumerate(y_top_fit):
+    if index > 0:
+        prev_val = y_top_fit[index - 1]
+        if value >= y_cen >= prev_val:
+            cutoff.append(index)
+        if value <= y_cen <= prev_val:
+            cutoff.append(index)
+    else:
+        continue
 
-c = 0.340 #where c = c_p
+y_split_1 = y_top_fit[:cutoff[0]:]
+y_split_2 = y_top_fit[cutoff[1]::]
+y_top_main = y_top_fit[cutoff[0]:cutoff[1]:]
+x_split_1 = x_fit[:cutoff[0]:]
+x_split_2 = x_fit[cutoff[1]::]
+x_top_main = x_fit[cutoff[0]:cutoff[1]:]
 
+#area_top = np.trapz(y_top_fit, x_fit)
+#area = area_top - area_bottom
+#top de l'aile
+"""l = np.linspace(0,1.51,1000)
+c = ((0.34-0.606)/2)*l+0.606"""
+c_p = 0.606
+#Calculate Q baby
+
+"""A_top = np.linspace(0,area_top*c_p**2,2000)
+A_bot = np.linspace(0,area_bottom*c_p**2,2000)
+Q_top = np.trapz((y_top_fit-y_cen)*c_p,A_top)
+Q_bot = np.trapz((y_bottom_fit-y_cen)*c_p,A_bot)
+Q_tot = Q_top - Q_bot
+print(f'Q = {Q_tot}')
+"""
 #Plot this shit
 fig = plt.figure()
 ax = fig.add_subplot(111)
-#ax.plot(x_top, y_top, color = 'Orange')
-ax.plot(x_fit, y_bottom_fit, color = 'Purple')
+ax.plot(x_split_1, y_split_1, color = 'Orange')
+ax.plot(x_split_2, y_split_2, color = 'Cyan')
+ax.plot(x_top_main, y_top_main, color = 'Blue')
 #ax.plot(x_bottom, y_bottom, color = 'blue')
-ax.plot(x_fit,y_top_fit, color = 'Purple')
-ax.scatter(0.49251, 0.046128, color = 'Green')
+ax.plot(x_fit,y_bottom_fit, color = 'Purple')
+#ax.scatter(0.49251, 0.046128, color = 'Green')
 ax.scatter(0.40575, 0.053017, color = 'Red')
 fig.tight_layout()
 fig.subplots_adjust(top=0.9)
@@ -62,5 +93,5 @@ ax.tick_params(axis="y",direction="in")
 ax.tick_params(axis="x",direction="in")
 ax.set_ylim([-0.25,0.25])
 ax.set_xlim([0,1])
-#plt.show()
-fig.savefig(f"Graph_{title}", bbox_inches='tight',dpi=600)
+plt.show()
+#fig.savefig(f"Graph_{title}", bbox_inches='tight',dpi=600)
