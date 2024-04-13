7153c520fcaaa070d9bb7b3c7b1977374dfac67e
FancyGuitarist <simf3010@gmail.com>
2024-04-13 17:23:08 -0400

    PEP8 + tau_max
    Ici c'est PEP8

--- Chargement.py
+++ Chargement.py
@@ -13,53 +13,27 @@ w = data['   Chargement (N/m)'].values
 
 V = integrate.cumtrapz(w,pos,initial = 0)
 M = integrate.cumtrapz(V,pos,initial=0)
+M_inv = M[::-1]
+V_inv = V[::-1]
 
 
-"""charge = []
-M = []
-fit_charge = poly.polyfit(pos,charge_m,20)"""
-"""for index, value in enumerate(charge_m):
-    charge.append(pos[index]*value)
-    M.append((pos[index]**2)*value)"""
-
-
-
-"""fit_M = poly.polyfit(pos,M,120)
-fit_charge = poly.polyfit(pos,charge,120)"""
-#x_fit = np.linspace(0,1.51,2000)
 def make_poly(fit_coeffs, symbol):
     y_fit = 0
     for index, value in enumerate(fit_coeffs):
         y_fit += value*(symbol**index)
     return y_fit
 
-#y_fit_charge = make_poly(fit_charge, x_fit)
-#y_fit_M = make_poly(fit_M,x_fit)
-#M = np.trapz(y_fit_charge,x_fit)
-#M_max = np.max(y_fit_M)
-#V_max = np.max(y_fit_charge)
-"""x = Symbol('x')
-sigma = make_poly(fit_charge,x)
-V = integrate(sigma,x)
-M = integrate(V, x)"""
-
-#area_M = np.trapz(y_fit_M, x_fit)
-#area_charge = np.trapz(y_fit_charge,x_fit)
-#print(f'M = {M}, V = {V_max}')
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(pos, V, color = 'Black')
-ax.plot(pos, M, color = 'Orange')
-ax.plot(pos, w, color = 'Blue')
-#ax.plot(x_fit, y_fit_charge, color = 'Red')
-#ax.plot(x_fit, y_fit_M, color = 'Blue')
+ax.plot(pos, V_inv, color = 'Green')
+#ax.plot(pos, M_inv, color = 'Orange')
+#ax.plot(pos, w, color = 'Blue')
 fig.tight_layout()
 fig.subplots_adjust(top=0.9)
 plt.xlabel("position")
 plt.ylabel('charge')
 ax.tick_params(axis="y",direction="in")
 ax.tick_params(axis="x",direction="in")
-ax.set_ylim([0,65])
+ax.set_ylim([0,80])
 ax.set_xlim([0,1.6])
 plt.show()
--- Etude_Cas.py
+++ Etude_Cas.py
@@ -2,15 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import numpy.polynomial.polynomial as poly
+from scipy import integrate
 
-#Open na noor
+# Open na noor
 title = 'MH114'
 data = pd.read_csv(f'{title}.dat', delim_whitespace=True)
 
 x = data['x'].values
 y = data['y'].values
 
-#Split Wing Ding ding in two halves
+# Split Wing Ding ding in two halves
 x_bottom = []
 x_top = []
 y_bottom = []
@@ -26,24 +27,27 @@ for index, value in enumerate(y):
     else:
         continue
 
-#Fit dem curves
-fit_bottom = poly.polyfit(x_bottom,y_bottom,18)
-fit_top = poly.polyfit(x_top,y_top,18)
-x_fit = np.linspace(0,1,2000)
+# Fit dem curves
+fit_bottom = poly.polyfit(x_bottom, y_bottom, 18)
+fit_top = poly.polyfit(x_top, y_top, 18)
+x_fit = np.linspace(0, 1, 2000)
 
-#Make the polynomial with the fit
+# Make the polynomial with the fit
+
+
 def make_poly(fit_values, x_values):
     y_fit = 0
     for index, value in enumerate(fit_values):
         y_fit += value*(x_values**index)
     return y_fit
 
+
 y_bottom_fit = make_poly(fit_bottom, x_fit)
 y_top_fit = make_poly(fit_top, x_fit)
 
 y_cen = 0.053017
 x_cen = 0.40575
-#Calculate Area of Wing Ding ding
+# Calculate Area of Wing Ding ding
 area_bottom = np.trapz(y_bottom_fit, x_fit)
 cutoff = []
 prev_val = 0
@@ -64,7 +68,7 @@ x_split_1 = x_fit[:cutoff[0]:]
 x_split_2 = x_fit[cutoff[1]::]
 x_top_main = x_fit[cutoff[0]:cutoff[1]:]
 
-#Area above centroid
+# Area above centroid
 area_top = np.trapz(y_top_fit, x_fit)
 Atot = area_top - area_bottom
 A2 = np.trapz(y_split_1, x_split_1)
@@ -76,6 +80,8 @@ print(f'Area over centroid v1 = {A1_V1} m^2/c^2')
 
 A1_V2 = np.trapz(y_top_main - y_cen, x_top_main)
 print(f'Area over centroid v2 = {A1_V2} m^2/c^2')
+
+# Centroid of new area
 x_int = np.trapz((y_top_main - y_cen)*x_top_main, x_top_main)
 x_bar = x_int/A1_V2
 
@@ -85,51 +91,91 @@ y_bar = y_int/A1_V2
 
 print(f'y coords of new centroid = {y_bar} m/c')
 
-#Centroid of new area
+# find Q
+Q = (y_bar-y_cen)*A1_V2
+print(f'Q = {Q}')
+
+# find V and M
+title2 = 'lift_2'
+data = pd.read_csv(f'{title2}.csv', delimiter=',')
+
+pos = data['Position (m)'].values
+w = data['   Chargement (N/m)'].values
+
+v = integrate.cumtrapz(w[::-1], pos, initial=0)
+V = np.trapz(w[::-1], pos)
+M = np.trapz(v, pos)
+
+print(f'V = {V}, M = {M}')
+# define what is missing for tau
+
+t = np.max(x_top_main) - np.min(x_top_main)
+print(f'x_max = {np.max(x_top_main)}, ')
+Ix = 9.8490*10**(-5)
 
 
+l = np.linspace(0, 1.51, len(v))
+c_p = 0.340
+c_r = 0.606
+c = (((c_r-c_p)/1.51)*l)+c_p
+tau = (v*Q)/(Ix*t*(c**2))
+tau_max = np.max(tau)
+i = 0
+for index, value in enumerate(tau):
+    if value == tau_max:
+        i = index
+        break
+print(f'tau_max = {tau_max/10**3} kPa with c = {c[i]} m')
 
-"""c = ((0.34-0.606)/2)*l+0.606"""
-c_p = 0.606
-#Calculate Q baby
+tau_rup = 600*10**3
 
-"""A_top = np.linspace(0,area_top*c_p**2,2000)
-A_bot = np.linspace(0,area_bottom*c_p**2,2000)
-Q_top = np.trapz((y_top_fit-y_cen)*c_p,A_top)
-Q_bot = np.trapz((y_bottom_fit-y_cen)*c_p,A_bot)
-Q_tot = Q_top - Q_bot
-print(f'Q = {Q_tot}')
-"""
-#Plot this shit
+# print(f'Contrainte de cisaillement = {tau/10**(3)} kPa')
+FS = tau_rup/tau_max
+
+# print(f'Facteur de sécurité en cisaillement : {FS}')
+
+
+# Plot this shit
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(x_split_1, y_split_1, color = 'Orange')
-ax.plot(x_split_2, y_split_2, color = 'Cyan')
-ax.plot(x_top_main, y_top_main, color = 'Blue')
-ax.plot(x_fit,y_bottom_fit, color = 'Purple')
-#ax.scatter(0.49251, 0.046128, color = 'Green') # Centroid with thickness
-ax.scatter(0.40575, 0.053017, color = 'Red') # Centroid of heart
+ax.plot(x_split_1, y_split_1, color='Orange')
+ax.plot(x_split_2, y_split_2, color='Orange')
+ax.plot(x_top_main, y_top_main, color='Blue')
+ax.plot(x_fit, y_bottom_fit, color='Orange')
+ax.scatter(x_bar, y_bar, color='Blue')
+# ax.scatter(0.49251, 0.046128, color = 'Green') # Centroid with thickness
+ax.scatter(x_cen, y_cen, color='Orange')  # Centroid of heart
 
-#Graph Setup Mumbo-Jumbo
+# Graph Setup Mumbo-Jumbo
 fig.tight_layout()
 fig.subplots_adjust(top=0.9)
 plt.xlabel("x/c")
 plt.ylabel('y/c')
-ax.tick_params(axis="y",direction="in")
-ax.tick_params(axis="x",direction="in")
-ax.set_ylim([-0.25,0.25])
-ax.set_xlim([0,1])
+ax.tick_params(axis="y", direction="in")
+ax.tick_params(axis="x", direction="in")
+ax.set_ylim([-0.25, 0.25])
+ax.set_xlim([0, 1])
 plt.show()
-#fig.savefig(f"Graph_{title}", bbox_inches='tight',dpi=600)
+# fig.savefig(f"Graph_{title}", bbox_inches='tight',dpi=600)
 
-#Plot top only
-plt.plot(x_top_main, y_top_main, color = 'blue')
-plt.scatter(x_bar, y_bar, color = 'blue')
-plt.tick_params(axis="y",direction="in")
-plt.tick_params(axis="x",direction="in")
+# Plot top only
+plt.plot(x_top_main, y_top_main, color='blue')
+plt.scatter(x_bar, y_bar, color='blue')
+plt.tick_params(axis="y", direction="in")
+plt.tick_params(axis="x", direction="in")
 plt.xlabel("x/c")
 plt.ylabel('y/c')
 plt.ylim([0, 0.3])
 plt.xlim([0, 1])
-#plt.savefig(f"Graph_top", bbox_inches='tight',dpi=600)
+# plt.savefig(f"Graph_top", bbox_inches='tight',dpi=600)
+plt.show()
+
+# Plot V on Wing
+plt.plot(l, tau, color='blue')
+plt.tick_params(axis="y", direction="in")
+plt.tick_params(axis="x", direction="in")
+plt.xlabel("l")
+plt.ylabel('tau')
+# plt.ylim([0, 0.3])
+plt.xlim([0, 1.51])
 plt.show()
